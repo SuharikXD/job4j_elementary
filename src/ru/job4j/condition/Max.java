b69dcd06d44130b2060cb4b42e118e66180f5b56
Данил <SuharikXD@yandex.ru>
2021-09-22 17:34:30 +0300

    1. Чтение кода и базовые ошибки [#40205]

--- src/ru/job4j/condition/Max.java
+++ src/ru/job4j/condition/Max.java
@@ -2,8 +2,11 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        int result = left > right ? left : right;
-        return result;
+        return left > right ? left : right;
+    }
+
+    public static int max(int left, int right, int middle) {
+        return left > max(left, middle) ? left : max(left, middle);
     }
 
     public static void main(String[] args) {
